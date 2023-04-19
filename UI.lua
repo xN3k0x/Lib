d784f2428478c5cc322fc989cec4bfd8c1d7ae99
N3ko <112410366+xN3k0x@users.noreply.github.com>
2023-04-19 08:06:44 +0700

    Update UI.lua

--- UI.lua
+++ UI.lua
@@ -31,8 +31,8 @@ local Library = {
     FontColor = Color3.fromRGB(255, 255, 255);
     MainColor = Color3.fromRGB(28, 28, 28);
     BackgroundColor = Color3.fromRGB(20, 20, 20);
-    AccentColor = Color3.fromRGB(0, 77, 255);
-    OutlineColor = Color3.fromRGB(0, 32, 108);
+    AccentColor = Color3.fromRGB(255, 46, 46);
+    OutlineColor = Color3.fromRGB(50, 50, 50);
     RiskColor = Color3.fromRGB(255, 50, 50),
 
     Black = Color3.new(0, 0, 0);
